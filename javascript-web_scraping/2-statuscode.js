c6e996bf9cbbf46b6093475ff4b5edc83391f57c
Aldo2303 <aldod03@gmail.com>
2023-09-20 19:39:06 -0700

    fix task

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 /*
-Display the status code of a GET request.
-The first argument is the URL to request (GET)
-The status code must be printed like this: code: <status code>
-You must use the module request
+Write a script that prints the title of a Star Wars movie where
+the episode number matches a given integer.
 */
+const filePath = process.argv[2];
 const request = require('request');
-const url = process.argv[2];
 
-request(url, (err, response) => {
+request(filePath, (err, response, body) => {
 	if (err) {
 		console.log(err);
 	}
-	const statusCode = response.statusCode;
-	console.log(`code: ${statusCode}`);
+	console.log('code:', response && response.statusCode);
 });
