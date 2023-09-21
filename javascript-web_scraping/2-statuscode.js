00f15155afc29c63efee9d7e8fb12275b3dee8c3
Aldo2303 <124457990+Aldo2303@users.noreply.github.com>
2023-09-20 23:56:09 -0300

    new changed

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,3 +5,10 @@ the episode number matches a given integer.
 */
 const filePath = process.argv[2];
 const request = require('request');
+
+request(filePath, (err, response) => {
+	if (err) {
+		console.log(err);
+	}
+	console.log('code: ' + response.StatusCode);
+});
